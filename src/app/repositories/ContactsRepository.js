3e300c5a419e2145b85c23b136f8895cde57eb4b
luisGuilhe <guilherme.aires.dias@gmail.com>
2024-01-16 23:16:22 -0300

    add method show and delete controller,repositorie

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,19 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
-    name: 'Luis Guilherme',
-    email: 'guilherme.aires.dias@gmail.com',
+    id: v4(),
+    name: 'Luis',
+    email: 'luis@gmail.com',
     phone: '12787123456',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Guilherme',
+    email: 'guilherme@gmail.com',
+    phone: '12787123456',
+    category_id: v4(),
   },
 ];
 
@@ -14,6 +21,19 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve, reject) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
